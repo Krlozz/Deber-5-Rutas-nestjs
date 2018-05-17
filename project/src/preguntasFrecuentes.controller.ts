aeb75d3e962b6e995b92df4e13878d58d5f4ab99
Krlozz Ayala <k_arlos210@hotmail.com>
2018-05-17 04:36:26 -0500

    añade Preguntas

--- project/src/preguntasFrecuentes.controller.ts
+++ project/src/preguntasFrecuentes.controller.ts
@@ -1,7 +1,35 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get, Post, Req, Res} from "@nestjs/common";
 
+const fs =require('fs');
+let preguntasHtml=fs.readFileSync(__dirname+'/html/preguntasFrecuentes.html', 'utf8');
 
 @Controller()
 export class PreguntasFrecuentesController {
+
+    propiedadesPreguntas: pregutasFrecuentes[] = []
+    @Post('anadir')
+    anadirPreguntas(@Req() req, @Res() res){
+        const params = req.query;
+        this.propiedadesPreguntas
+            .push(new pregutasFrecuentes(params.pregunta,params.respuesta));
+
+        return res.send(this.propiedadesPreguntas);
+    }
+
+    @Get('mostrar')
+    mostrarPreguntas(@Res () response){
+
+
+
+
+    }
+
+
+}
+
+export class pregutasFrecuentes{
+    constructor(public pregunta: string,
+                public respuesta: string){
+    }
 
 }
