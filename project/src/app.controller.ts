1ebcb3007c3e1938fa74fcaff7eab94dc5d75e51
Krlozz Ayala <k_arlos210@hotmail.com>
2018-05-17 01:40:51 -0500

    modificación en controllers
    app.controller modificado
    inicio.controller modificado sin status

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller, Req, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+
+const fs = require('fs')
+let contador = 0;
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  //constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
-  }
+    @Get()
+    root(): string {
+        const nombre = 'Carlos Ayala';
+        let html = fs.readFileSync(
+            __dirname+'/html/contenido.html',
+            'utf8');
+        html = html.replace('{{nombre}}',nombre);
+        return html;
+    }
 }
--- project/src/inicio.controller.ts
+++ project/src/inicio.controller.ts
@@ -1,7 +1,28 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get, HttpCode, Req, Res} from "@nestjs/common";
 
+const fs = require('fs');
+
+const nombre = 'Carlos Ayala';
 
 @Controller()
 export class InicioController {
+    @Get('Home')
+    @HttpCode(200)
+    mostrar(@Req()request, @Res() response){
+        let htmlFooter=fs.readFileSync(
+            __dirname+'/html/footer.html',
+            'utf8');
+        let htmlHeader= fs.readFileSync(
+            __dirname+'/html/header.html',
+            'utf8');
+        let htmlContenido= fs.readFileSync(
+            __dirname+'/html/contenido.html',
+            'utf8');
+        htmlContenido = htmlContenido.replace('{{nombre}}',nombre);
 
+        let cadena= htmlHeader + htmlContenido + htmlFooter;
+        console.log(response.status(200).send(cadena));
+        return response.status(200).send(cadena);
+
+    }
 }
