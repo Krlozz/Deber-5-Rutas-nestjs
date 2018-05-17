0e4df90169e9f45d2a450729794264b4defabd9c
Krlozz Ayala <k_arlos210@hotmail.com>
2018-05-17 05:31:45 -0500

    añadido Get
    Muestra las preguntas y respuestas añadidas con Post, con el formato indicado

--- project/src/preguntasFrecuentes.controller.ts
+++ project/src/preguntasFrecuentes.controller.ts
@@ -1,35 +1,40 @@
 import {Controller, Get, Post, Req, Res} from "@nestjs/common";
 
 const fs =require('fs');
-let preguntasHtml=fs.readFileSync(__dirname+'/html/preguntasFrecuentes.html', 'utf8');
+let htmlPregFre=fs.readFileSync(__dirname+'/html/preguntasFrecuentes.html', 'utf8');
+
+let contador = 0;
 
 @Controller()
 export class PreguntasFrecuentesController {
 
-    propiedadesPreguntas: pregutasFrecuentes[] = []
+    propiedadesPreguntas: preguntasFrecuentes[] = []
     @Post('anadir')
     anadirPreguntas(@Req() req, @Res() res){
+        contador++;
         const params = req.query;
         this.propiedadesPreguntas
-            .push(new pregutasFrecuentes(params.pregunta,params.respuesta));
+            .push(new preguntasFrecuentes(params.pregunta,params.respuesta));
+
+        /*htmlPregFre = htmlPregFre.replace("{{pregunta}}",params.pregunta);
+        htmlPregFre = htmlPregFre.replace("{{respuesta}}",params.respuesta);*/
+        htmlPregFre = htmlPregFre.concat('<h1> {{contador}}. {{pregunta}}</h1>');
+        htmlPregFre = htmlPregFre.replace("{{pregunta}}",params.pregunta);
+        htmlPregFre = htmlPregFre.replace("{{contador}}",contador);
+        htmlPregFre = htmlPregFre.concat('<p> {{respuesta}} </p>');
+        htmlPregFre = htmlPregFre.replace("{{respuesta}}",params.respuesta);
 
         return res.send(this.propiedadesPreguntas);
     }
 
     @Get('mostrar')
-    mostrarPreguntas(@Res () response){
-
-
-
-
+    mostrarPreguntas(@Res () res){
+        return res.status(200).send(htmlPregFre);
     }
-
-
 }
 
-export class pregutasFrecuentes{
+export class preguntasFrecuentes{
     constructor(public pregunta: string,
                 public respuesta: string){
     }
-
 }
