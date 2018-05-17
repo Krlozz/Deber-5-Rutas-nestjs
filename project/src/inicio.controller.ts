8233f194eaf94f2e624de7242efd154a06867d7f
Krlozz Ayala <k_arlos210@hotmail.com>
2018-05-17 02:06:53 -0500

    Terminado inicio.controller

--- project/src/inicio.controller.ts
+++ project/src/inicio.controller.ts
@@ -1,14 +1,12 @@
 import {Controller, Get, HttpCode, Req, Res} from "@nestjs/common";
 
 const fs = require('fs');
-
 const nombre = 'Carlos Ayala';
 
 @Controller()
 export class InicioController {
     @Get('Home')
-    @HttpCode(200)
-    mostrar(@Req()request, @Res() response){
+    buscarHtml(@Req() req, @Res() res){
         let htmlFooter=fs.readFileSync(
             __dirname+'/html/footer.html',
             'utf8');
@@ -20,9 +18,26 @@ export class InicioController {
             'utf8');
         htmlContenido = htmlContenido.replace('{{nombre}}',nombre);
 
-        let cadena= htmlHeader + htmlContenido + htmlFooter;
-        console.log(response.status(200).send(cadena));
-        return response.status(200).send(cadena);
+        let contenidoTotal= htmlHeader + htmlContenido + htmlFooter;
+        // Modificación para status
+        /*console.log(res.status(200).send(contenidoTotal));
+        return res.status(200).send(contenidoTotal);*/
 
+        const existeContenido = (contenidoTotal);
+
+        // Todo sale bien
+        if(existeContenido) {
+            console.log(res.status(200).send(contenidoTotal));
+            return res.status(200).send(contenidoTotal);
+        }else {
+            return res.status(500).send({mensaje: 'Existen errores', status: 500})
+        }
+        // Retorna error
+        /*if(existeContenido==null) {
+            console.log(res.status(200).send(contenidoTotal));
+            return res.status(200).send(contenidoTotal);
+        }else {
+            return res.status(500).send({mensaje: 'Existen errores', status: 500})
+        }*/
     }
 }
